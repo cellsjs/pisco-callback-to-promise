008c571d76461dae157bc9c5ec575cb993d24186
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-06-13 12:32:06 +0200

    test(): include test for error
    Test for error execution
    
    [QPA-110]

--- test/index.js
+++ test/index.js
@@ -2,17 +2,26 @@
 
 const expect = require('chai').expect;
 const pctp = require('../index');
-const childProcess = require('child_process');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
 
 /*global define, describe, it*/
 describe('Testing the callbacks to promises', () => {
   it('Run the pwd command', (done) => {
-    pctp.c2p(childProcess.exec, 'pwd')
+    pctp.c2p(exec, 'pwd')
       .then(stdout => {
         expect(stdout).contain('pisco-callback-to-promise');
       })
       .then(() => done())
       .catch(done);
+  });
+  it('Run the pwd command with error', (done) => {
+    pctp.c2p(exec, 'chmod', '755 noFile.txt')
+      .catch((error) => {
+        expect(error).not.equal(null);
+        expect(error.toString()).contain('Command failed: chmod');
+        done();
+      });
   });
 });
