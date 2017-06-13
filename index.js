2c53fa137180d5a3a496aa597ce3f86e4e06bbc8
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2017-06-13 11:08:26 +0200

    refactor() Refactor code for compatibility with node 4
    [QPA-110]

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ module.exports = {
   c2p(fn) {
     return new Promise((resolve, reject) => {
       const args = [].slice.call(arguments, 1);
-      fn.apply(this, args.concat(function(err) {
+      fn.apply(null, args.concat(function(err) {
         var resultArgs = [].slice.call(arguments, 1);
         if (err) {
           reject(err);
         } else {
-          resolve(...resultArgs);
+          resolve.apply(null, resultArgs);
         }
       }));
     });
